335a635d10e441ac53205e5ab0f03d030fe99bdc
Peter Aronoff <peter@aronoff.org>
2023-02-08 13:15:25 -0500

    feat: clean up tests and add CI on GitHub

--- go/investment/investment_test.go
+++ go/investment/investment_test.go
@@ -9,6 +9,7 @@ func getBank() *investment.Bank {
 	b := investment.NewBank()
 	b.AddExchangeRate("EUR", "USD", 1.2)
 	b.AddExchangeRate("USD", "KRW", 1100)
+	b.AddExchangeRate("EUR", "KRW", 1100)
 
 	return b
 }
@@ -43,16 +44,15 @@ func TestDivision(t *testing.T) {
 func TestAddition(t *testing.T) {
 	t.Parallel()
 
-	var portfolio investment.Portfolio
-	var portfolioInDollars *investment.Money
-
 	fiveDollars := investment.NewMoney(5, "USD")
 	tenDollars := investment.NewMoney(10, "USD")
 	fifteenDollars := investment.NewMoney(15, "USD")
 
+	var portfolio investment.Portfolio
 	portfolio = portfolio.Add(fiveDollars)
 	portfolio = portfolio.Add(tenDollars)
 
+	var portfolioInDollars *investment.Money
 	portfolioInDollars, err := portfolio.Evaluate(getBank(), "USD")
 	if err != nil {
 		t.Fatalf("portfolio.Add(fiveDollars) + portfolio.Add(tenDollars) has error [%+v]; want nil", err)
@@ -69,11 +69,10 @@ func TestAddition(t *testing.T) {
 func TestAdditionOfDollarsAndEuros(t *testing.T) {
 	t.Parallel()
 
-	var portfolio investment.Portfolio
-
 	fiveDollars := investment.NewMoney(5, "USD")
 	tenEuros := investment.NewMoney(10, "EUR")
 
+	var portfolio investment.Portfolio
 	portfolio = portfolio.Add(fiveDollars)
 	portfolio = portfolio.Add(tenEuros)
 
@@ -95,11 +94,10 @@ func TestAdditionOfDollarsAndEuros(t *testing.T) {
 func TestAdditionOfDollarsAndWon(t *testing.T) {
 	t.Parallel()
 
-	var portfolio investment.Portfolio
-
 	oneDollar := investment.NewMoney(1, "USD")
 	elevenHundredWon := investment.NewMoney(1100, "KRW")
 
+	var portfolio investment.Portfolio
 	portfolio = portfolio.Add(oneDollar)
 	portfolio = portfolio.Add(elevenHundredWon)
 
@@ -118,26 +116,7 @@ func TestAdditionOfDollarsAndWon(t *testing.T) {
 	)
 }
 
-func TestEvaluationWithMissingExchangeRates(t *testing.T) {
-	t.Parallel()
-
-	var portfolio investment.Portfolio
-
-	oneDollar := investment.NewMoney(1, "USD")
-	oneEuro := investment.NewMoney(1, "EUR")
-	oneWon := investment.NewMoney(1, "KRW")
-
-	portfolio = portfolio.Add(oneDollar)
-	portfolio = portfolio.Add(oneEuro)
-	portfolio = portfolio.Add(oneWon)
-
-	_, got := portfolio.Evaluate(getBank(), "Kalganid")
-	if got == nil {
-		t.Error("portfolio.Evaluate(\"Kalganid\") error should not be nil")
-	}
-}
-
-func TestSuccessfulConversion(t *testing.T) {
+func TestSuccessfulConvert(t *testing.T) {
 	t.Parallel()
 
 	bank := investment.NewBank()
@@ -159,4 +138,61 @@ func TestSuccessfulConversion(t *testing.T) {
 		got.Amount(),
 		want.Amount(),
 	)
+}
+
+func TestUnsuccessfulConvert(t *testing.T) {
+	t.Parallel()
+
+	bank := investment.NewBank()
+	bank.AddExchangeRate("EUR", "USD", 1.2)
+	tenEuros := investment.NewMoney(10, "EUR")
+	_, err := bank.Convert(tenEuros, "KAL")
+
+	if err == nil {
+		t.Error("bank.Convert(tenEuros, \"KAL\") is nil; want error")
+	}
+}
+
+func TestSuccessfulMultiCurrencyEvaluation(t *testing.T) {
+	t.Parallel()
+
+	oneDollar := investment.NewMoney(1, "USD")
+	oneEuro := investment.NewMoney(1, "EUR")
+	elevenHundredWon := investment.NewMoney(1100, "KRW")
+
+	var portfolio investment.Portfolio
+	portfolio = portfolio.Add(oneDollar)
+	portfolio = portfolio.Add(oneEuro)
+	portfolio = portfolio.Add(elevenHundredWon)
+
+	want := 3300.0
+	got, err := portfolio.Evaluate(getBank(), "KRW")
+	if err != nil {
+		t.Fatal("portfolio.Evaluate(getBank(), \"KRW\") error should be nil")
+	}
+
+	amountCheck(
+		t,
+		"portfolio.Evaluate(getBank(), \"KRW\")",
+		got.Amount(),
+		want,
+	)
+}
+
+func TestUnsuccessfulMultiCurrencyEvaluation(t *testing.T) {
+	t.Parallel()
+
+	var portfolio investment.Portfolio
+	oneDollar := investment.NewMoney(1, "USD")
+	portfolio = portfolio.Add(oneDollar)
+
+	got, err := portfolio.Evaluate(getBank(), "FOO")
+
+	if got != nil {
+		t.Error("portfolio.Evaluate(getBank(), \"FOO\") *Money should be nil")
+	}
+
+	if err == nil {
+		t.Error("portfolio.Evaluate(getBank(), \"FOO\") error should not be nil")
+	}
 }
